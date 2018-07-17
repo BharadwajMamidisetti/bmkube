e40fb697d14213c303489424bfbac2d1fcf30fa3
bmgituser <34877658+sudhabharadwajm@users.noreply.github.com>
2018-07-17 11:38:45 -0700

    get ip address of pod in kubernetes updated

--- rx100lab1/getpodip.sh
+++ rx100lab1/getpodip.sh
@@ -1,1 +1,7 @@
+# Get pod ip address from kubectl command
+kubectl get pod -o=custom-columns=Name:.metadata.name,hostIP:.status.hostIP,podIP:status.podIP
+# Get pod id from docker API and jq.
 curl -s --unix-socket /var/run/docker.sock http:/networks/bridge | jq -r '.IPAM.Config[].Subnet'
+
+# Get POD ip address from docker0 bridge, via docker network sub command
+docker network inspect bridge | jq -r '.[] | select(.Name=="bridge").IPAM.Config[].Subnet
